0a7d6ff545204dd2c8ca12e27d08da9e9865548f
JakeLegacyDECLA <a01712823@tec.mx>
2026-02-12 13:07:08 -0600

    Agregar funciones de javascript

---
+++ Lab3/intro.js
@@ -0,0 +1,59 @@
+// consola (log, info, warn, error, assert)
+console.log("Hello World!");
+console.info("Creado en el 2009");
+console.warn("Es adictivo");
+console.error("Los tanques no deben ir atrás");
+console.assert(1 == true);
+
+//== Operador "estrictamente igual" de comparación de valor y tipo 
+console.assert(1 === true); 
+
+//== variables, constantes
+
+//Forma antigua de declarar variables, tiene mayor alcance por lo que no se recomienda
+var personaje_1 = "Gwen"
+
+//Forma moderna de declarar variables, solo vive dentro del ambito donde es declarada
+let personaje_2 = "Mordekaiser";
+
+const precio_skin = 300;
+
+//Alcance de las variables
+{
+    var personaje_3 = "Jax";
+    let personaje_4 = "Garen"; //Esta variable solo existe entre las llaves, por su alcance
+}
+
+console.log(personaje_3);
+//console.log(personaje_4);
+
+// alert, prompt, confirm
+
+alert("No juegues esto por favor");
+const personaje_favorito= prompt("¿Cuál es tu personaje favorito?");
+console.info("Personaje favorito: " + personaje_favorito);
+const hoy_hay_juego = confirm("¿Un jueguito?");
+if (hoy_hay_juego) {
+    descargar();
+} else {
+    console.info("Buen día");
+}
+
+//Funciones tradicionales
+function descargar() {
+    window.location.href = "https://www.leagueoflegends.com/es-mx/";
+}
+
+// Funciones modernas
+
+() => {}
+
+document.getElementById("boton_desinstalar").onclick = () => {
+    alert("jojojo no se puede desinstalar");
+}
+
+const iniciar_partida = () => {
+    alert("Iniciar partida...");
+}
+
+iniciar_partida();
