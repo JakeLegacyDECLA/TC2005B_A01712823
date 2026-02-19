13275dbe9ab023dc7bac898774ac430851a209a8
JakeLegacyDECLA <a01712823@tec.mx>
2026-02-19 12:29:22 -0600

    Funciones asincronicas

---
+++ LabsNEW/app.js
@@ -0,0 +1,16 @@
+console.log("hola desde node!");
+
+//Esto nos permite entrar al DOM con node.js
+const filesystem = require('fs');
+
+//Escribir archivo
+filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+//Sort Asincronico, cuando pasen 5000 milisegundos se imprime el 5000 y etc.
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
