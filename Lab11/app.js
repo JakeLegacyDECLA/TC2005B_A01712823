76d649d74fda464b85df780da91878ac48b739a4
JakeLegacyDECLA <a01712823@tec.mx>
2026-02-26 12:35:28 -0600

    Explicacion de Lab12 en clase con lo que llevabamos de Lab11

--- Lab11/app.js
+++ Lab11/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-const ruta_gwen = require('./Routes/gwen.routes');
-app.use('/personajes/gwen', ruta_gwen);
-
 const rutas_personajes = require('./Routes/personajes.routes');
 app.use('/personajes', rutas_personajes);
-
 
 app.use((request, response, next) => {
     response.status(404).send("La ruta no existe");
