91e4ca4605daa75bab231cad32f38de826d83248
JakeLegacyDECLA <a01712823@tec.mx>
2026-03-03 12:27:58 -0600

    Hacer inicio de sesión

---
+++ Lab14/controllers/users.controller.js
@@ -0,0 +1,12 @@
+exports.get_login = (request, response, next) => {
+    response.render('login');
+};
+
+exports.post_login = (request, response, next) => {
+    request.session.username = request.body.username;
+    response.redirect('/personajes');
+};
+
+exports.get_logout = () => {
+
+};
